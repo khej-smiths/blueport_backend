3ebc0277d74cd3fe9bd9f336360a9b9b727a8629
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-08 00:47:19 +0900

    FIX: (post -create) 임시 리턴 추가

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,9 +1,11 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { Post, PostInputType } from './post.model';
 import { input } from 'src/common/consts';
+import { PostService } from './post.service';
 
 @Resolver('Post')
 export class PostResolver {
+  constructor(private readonly postService: PostService) {}
   @Query(() => Post)
   readPost(): Post {
     return {
@@ -14,11 +16,7 @@ export class PostResolver {
   }
 
   @Mutation(() => Post)
-  createPost(@Args(input) input: PostInputType): Post {
-    return {
-      id: 1,
-      title: '1',
-      content: '1',
-    };
+  async createPost(@Args(input) input: PostInputType): Promise<Post> {
+    return await this.postService.createPost(input);
   }
 }
