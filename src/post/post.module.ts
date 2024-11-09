08a7c0203262d10181f845451fea1b728ba8f107
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-09 23:51:04 +0900

    FIX: (Post) User 추가 적용

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  providers: [PostResolver, PostService]
+  imports: [UserModule],
+  providers: [PostResolver, PostService],
 })
 export class PostModule {}
--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,12 +1,24 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { Post, PostInputType } from './post.model';
 import { input } from 'src/common/consts';
 import { PostService } from './post.service';
 import { ReadPostInputDto } from './dtos/read-post.dto';
+import { User } from 'src/user/user.model';
+import { UserService } from 'src/user/user.service';
 
-@Resolver('Post')
+@Resolver(() => Post)
 export class PostResolver {
-  constructor(private readonly postService: PostService) {}
+  constructor(
+    private readonly postService: PostService,
+    private readonly userService: UserService,
+  ) {}
   @Query(() => Post)
   async readPost(@Args(input) input: ReadPostInputDto): Promise<Post> {
     return await this.postService.readPost(input);
@@ -15,5 +27,13 @@ export class PostResolver {
   @Mutation(() => Post)
   async createPost(@Args(input) input: PostInputType): Promise<Post> {
     return await this.postService.createPost(input);
+  }
+
+  @ResolveField('writer', () => User, { nullable: true })
+  async readUser(@Parent() post: Post): Promise<User | null> {
+    console.log(post);
+    return await this.userService.readUserByOption({
+      postId: post.id,
+    });
   }
 }
