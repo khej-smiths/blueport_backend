a4325d7b2e773d953c2784bc3e367f3b3780fc67
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-10 00:35:06 +0900

    FIX: (User) 에러 로직 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.model';
+import { CustomGraphQLError } from 'src/common/error';
 
 @Injectable()
 export class UserService {
-  async readUserByOption(option: { postId?: number }): Promise<User> {
-    if (Object.keys(option).length > 1) {
-      throw new Error('too much options');
+  async readUserByOption(option: {
+    postId?: number;
+    userId?: number;
+  }): Promise<User> {
+    const keyListLength = Object.keys(option).length;
+
+    if (keyListLength === 0) {
+      throw new Error('키가 필수로 들어가야함');
+    } else if (keyListLength > 1) {
+      // throw new Error('옵션이 잘못 설정됨');
+      throw new CustomGraphQLError('옵션이 잘못 설정됨', {
+        extensions: { code: 'ERR_NO_USER' },
+      });
     }
 
     return {
