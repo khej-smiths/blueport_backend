30d41746a10d62a8f7cdf4d5e371c5aef7b478fd
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-10 01:05:00 +0900

    FIX: (APP) Graphql에 환경변수 관련 설명 주석 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,8 +22,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      imports: [ConfigModule],
-      inject: [ConfigService],
+      imports: [ConfigModule], // 모듈을 다른 모듈에서 사용할 수 있도록 가져오는 역할(모듈간의 의존성 관계를 설정하는데 사용)
+      inject: [ConfigService], // 의존성 주입에서 특정 클래스 또는 서비스를 명시적으로 주입받고자 할 때 사용하는 배열. 주로 팩토리 함수 또는 커스텀 프로바이더에서 사용
       useFactory: async (configService: ConfigService) => {
         console.log(configService);
         return {
