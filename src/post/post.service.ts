6d1ff1995d57d19ffdda62e1b755792b15f0ed4a
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-16 11:42:30 +0900

    FIX: (Post) 게시글 조회 로직에 에러 작업 진행중

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -8,22 +8,39 @@ import { CustomGraphQLError } from 'src/common/error';
 export class PostService {
   constructor(private readonly postRepository: PostRepository) {}
   async readPost(input: ReadPostInputDto): Promise<Post> {
-    const postList = await this.postRepository.readPostList({ id: input.id });
+    try {
+      const postList = await this.postRepository.readPostList({ id: input.id });
 
-    if (postList.length === 0) {
-      throw new CustomGraphQLError('게시글 조회에 실패했습니다.', {
-        extensions: {
-          code: 'NO_DATA',
-        },
-      });
-    } else if (postList.length > 1) {
-      throw new CustomGraphQLError('선택된 게시글이 여러개입니다.', {
-        extensions: {
-          code: 'MULTIPLE_DATA',
-        },
-      });
-    } else {
-      return postList[0];
+      if (postList.length === 0) {
+        throw new CustomGraphQLError('게시글 조회에 실패했습니다.', {
+          extensions: {
+            code: 'NO_DATA',
+          },
+        });
+      } else if (postList.length > 1) {
+        throw new CustomGraphQLError('선택된 게시글이 여러개입니다.', {
+          extensions: {
+            code: 'MULTIPLE_DATA',
+          },
+        });
+      } else {
+        return postList[0];
+      }
+    } catch (e) {
+      if (
+        [
+          '게시글 조회에 실패했습니다.',
+          '선택된 게시글이 여러개입니다.',
+        ].includes(e.message)
+      ) {
+        throw e;
+      } else {
+        throw new CustomGraphQLError('어떤 에러인지모르겠어요', {
+          extensions: {
+            code: 'MULTIPLE_DATA',
+          },
+        });
+      }
     }
   }
 
