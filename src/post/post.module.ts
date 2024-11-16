51119f1a25e21f8dc6f20f54fd503c7842b24df0
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-16 11:29:26 +0900

    FIX: (Post) Repository 적용

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
 import { UserModule } from 'src/user/user.module';
+import { PostRepository } from './post.repository';
 
 @Module({
   imports: [UserModule],
-  providers: [PostResolver, PostService],
+  providers: [PostResolver, PostService, PostRepository],
 })
 export class PostModule {}
