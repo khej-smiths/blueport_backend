7bf0dae8d8e29b1ee0fb04b047d500b43eb42ea6
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-16 11:29:52 +0900

    FIX: (Post) 목록 호출 함수 추가

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -12,6 +12,7 @@ import { PostService } from './post.service';
 import { ReadPostInputDto } from './dtos/read-post.dto';
 import { User } from 'src/user/user.model';
 import { UserService } from 'src/user/user.service';
+import { ReadPostListInputDto } from './dtos/read-post-list.dto';
 
 @Resolver(() => Post)
 export class PostResolver {
@@ -19,6 +20,14 @@ export class PostResolver {
     private readonly postService: PostService,
     private readonly userService: UserService,
   ) {}
+
+  @Query(() => [Post])
+  async readPostList(
+    @Args(input) input: ReadPostListInputDto,
+  ): Promise<Array<Post>> {
+    return [];
+  }
+
   @Query(() => Post)
   async readPost(@Args(input) input: ReadPostInputDto): Promise<Post> {
     return await this.postService.readPost(input);
