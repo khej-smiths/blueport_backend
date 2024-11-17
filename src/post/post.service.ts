ac1f2bfb3c618813a2d4acf65f0557302b744fe3
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-11-17 23:19:58 +0900

    FIX: (게시글 조회) 에러 처리 수정

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { Post, PostInputType } from './post.model';
 import { ReadPostInputDto } from './dtos/read-post.dto';
 import { PostRepository } from './post.repository';
-import { CustomGraphQLError } from 'src/common/error';
+import { CustomGraphQLError, ERROR_CODE_READ_POST } from 'src/common/error';
 
 @Injectable()
 export class PostService {
-  constructor(private readonly postRepository: PostRepository) {}
+  private readonly ERROR_CODE: Record<string, typeof ERROR_CODE_READ_POST>;
+
+  constructor(private readonly postRepository: PostRepository) {
+    this.ERROR_CODE = {
+      ERROR_CODE_READ_POST,
+    };
+  }
+
   async readPost(input: ReadPostInputDto): Promise<Post> {
     try {
       const postList = await this.postRepository.readPostList({ id: input.id });
@@ -14,13 +21,13 @@ export class PostService {
       if (postList.length === 0) {
         throw new CustomGraphQLError('게시글 조회에 실패했습니다.', {
           extensions: {
-            code: 'NO_DATA',
+            code: this.ERROR_CODE.ERROR_CODE_READ_POST.NO_DATA,
           },
         });
       } else if (postList.length > 1) {
         throw new CustomGraphQLError('선택된 게시글이 여러개입니다.', {
           extensions: {
-            code: 'MULTIPLE_DATA',
+            code: this.ERROR_CODE.ERROR_CODE_READ_POST.MULTIPLE_DATA,
           },
         });
       } else {
@@ -28,16 +35,17 @@ export class PostService {
       }
     } catch (e) {
       if (
-        [
-          '게시글 조회에 실패했습니다.',
-          '선택된 게시글이 여러개입니다.',
-        ].includes(e.message)
+        // 예측한 에러케이스인 경우, 위에서 잡은 에러를 그대로 던지기
+        // 그 외 케이스인 경우 그대로 던지기
+        Object.keys(this.ERROR_CODE.ERROR_CODE_READ_POST).includes(
+          e.extensions?.code,
+        )
       ) {
         throw e;
       } else {
-        throw new CustomGraphQLError('어떤 에러인지모르겠어요', {
+        throw new CustomGraphQLError(e, {
           extensions: {
-            code: 'MULTIPLE_DATA',
+            code: 'UNEXPECTED_ERROR',
           },
         });
       }
