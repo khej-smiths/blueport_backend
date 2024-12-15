1b9009d24db7728ac34dd5b919bff46418b82fad
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-12-16 00:13:44 +0900

    FIX: (log) requestId에 uuid 적용, timestamp찍히도록 ConsoleLogger 상속으로 수정, 로그 전체 배열에 쌓도록 로직 수정

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,23 +1,90 @@
-import { Injectable, LoggerService } from '@nestjs/common';
+import { ConsoleLogger, Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class CustomLogger implements LoggerService {
+export class CustomLogger extends ConsoleLogger {
   private readonly requestId: string;
+  private email: string;
+  private logList: Array<string> = [];
 
   constructor() {
-    this.requestId = new Date().toISOString();
-    this.log('requestId');
+    super();
+    this.requestId = uuidv4();
+    this.customLog('start', {
+      className: this.constructor.name,
+      functionName: 'constructor',
+    });
   }
 
-  log(message: string) {
-    console.log(this.requestId, message);
+  setEmail(email: string) {
+    this.email = email;
+    this.customLog('set email', {
+      className: this.constructor.name,
+      functionName: this.setEmail.name,
+    });
   }
 
-  error(message: string) {
-    console.error(this.requestId, message);
+  customLog(
+    message: any,
+    option?: { className?: string; functionName?: string },
+  ) {
+    const prefix = this.getPrefix(option);
+    const processedMessage = this.processMessage(message);
+    this.logList.push(`${prefix} ${processedMessage}`);
+    this.log(processedMessage, prefix);
   }
 
-  warn(message: string) {
-    console.warn(this.requestId, message);
+  // TODO Error, Warn 추가
+  // customError(
+  //   message: string,
+  //   option?: { className?: string; functionName: string },
+  // ) {
+  //   const prefix = this.getPrefix(option);
+  //   console.error(prefix, message);
+  // }
+
+  // customWarn(
+  //   message: string,
+  //   option?: { className?: string; functionName: string },
+  // ) {
+  //   const prefix = this.getPrefix(option);
+  //   console.warn(prefix, message);
+  // }
+
+  destroy() {
+    this.customLog(this.logList, {
+      className: this.constructor.name,
+      functionName: this.destroy.name,
+    });
+    this.logList = [];
+  }
+
+  /**
+   * 로그를 구분할 수 있는 prefix를 생성 후 반환하는 함수
+   * @param option (optional) 로그의 prefix를 위한 추가 정보
+   * @returns 로그를 구분할 수 있는 prefix
+   */
+  private getPrefix(option?: { className?: string; functionName?: string }) {
+    // constructor에서 생성된 requestId를 prefix의 가장 앞에 부여
+    let prefix = this.requestId;
+
+    // option이 존재할 경우 추가 정보를 prefix에 부여
+    if (option) {
+      if (option.className) prefix = `${prefix} # ${option.className}`;
+      if (option.functionName) prefix = `${prefix} # ${option.functionName}`;
+    }
+
+    // 이메일이 존재할 경우 prefix에 부여
+    if (this.email) prefix = `${prefix} # ${this.email}`;
+
+    // prefix
+    return `[${prefix}]`;
+  }
+
+  private processMessage(message: any): string {
+    if (typeof message === 'object') {
+      return JSON.stringify(message);
+    }
+    return message;
   }
 }
