8ced1c438070894a8c67efded28c6cb6cd124f18
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-12-16 00:12:37 +0900

    FIX: (log) 글로벌 인터셉터에 로그 셋팅 추가

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LoggerStorage } from './logger-storage';
+import { LoggerInterceptor } from './logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  providers: [LoggerStorage],
+  providers: [
+    LoggerStorage,
+    { provide: APP_INTERCEPTOR, useClass: LoggerInterceptor },
+  ],
   exports: [LoggerStorage],
 })
 export class LoggerModule {}
