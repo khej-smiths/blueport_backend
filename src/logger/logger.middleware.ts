a9969040ad35f87c3fa2f0b43c08953c53d95912
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-12-15 23:12:46 +0900

    FIX: (middleware) 로거 등록

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,11 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { LoggerStorage } from './logger-storage';
+import { CustomLogger } from './logger';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  constructor() {}
+  constructor(private readonly loggerStorage: LoggerStorage) {}
 
   use(req: Request, res: Response, next: NextFunction) {
-    next();
+    this.loggerStorage.run({ customLogger: new CustomLogger() }, () => {
+      next();
+    });
   }
 }
