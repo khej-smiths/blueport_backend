66a3554778f91e58fd9578f6ab1b8b2369478f2a
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-12-15 23:00:13 +0900

    ADD: (user) repository 추가

---
+++ src/user/user.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.model';
+import { getObjectKeysByGeneric } from 'src/common/consts';
+
+type ReadUserOption = {
+  id?: number;
+};
+
+@Injectable()
+export class UserRepository {
+  private readonly USER_LIST: Array<User> = Array.from(
+    { length: 10 },
+    (_, index: number) => {
+      return {
+        id: index,
+        name: `name ${index}`,
+      };
+    },
+  );
+
+  async readUserList(option: ReadUserOption): Promise<Array<User>> {
+    const optionKeyList = getObjectKeysByGeneric<ReadUserOption>(option);
+
+    return this.USER_LIST.filter((user) => {
+      return (
+        optionKeyList.filter((optionKey) => {
+          return user[optionKey] === option[optionKey];
+        }).length === optionKeyList.length
+      );
+    });
+  }
+}
