6f47cdda39a19cd116d8055d62f7b2a6508efeeb
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-09 15:14:01 +0900

    FIX: (user) 필드 추가, TypeORM 적용

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,16 +1,17 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import { CommonEntity } from 'src/common/common.entity';
+import { Column, Entity } from 'typeorm';
 
 @ObjectType({ isAbstract: true })
 @InputType({ isAbstract: true })
-abstract class IUser {
-  @Field(() => Int, { description: '유저의 id' })
-  id: number;
-
+abstract class IUser extends CommonEntity {
   @Field(() => String, { description: '유저의 이름' })
+  @Column({ type: 'varchar', length: 256, comment: '유저의 이름' })
   name: string;
 }
 
 @ObjectType()
+@Entity()
 export class User extends IUser {}
 
 @InputType()
