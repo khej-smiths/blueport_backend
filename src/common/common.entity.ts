14229403cd4b95cdc015cc5ad340b011e82ccd21
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-09 15:13:23 +0900

    ADD: (db) 공통사항들 묶어서 클래스로 셋팅

---
+++ src/common/common.entity.ts
@@ -0,0 +1,28 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Field, Int } from '@nestjs/graphql';
+
+export abstract class CommonEntity {
+  @PrimaryGeneratedColumn()
+  @Field(() => Int, { description: 'id' })
+  id: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  @Field(() => Date, { description: '데이터의 생성 날짜' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  @Field(() => Date, { description: '데이터의 업데이트 날짜' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  @Field(() => Date, {
+    description: '데이터의 삭제 날짜(soft)',
+    nullable: true,
+  })
+  deletedAt?: Date;
+}
