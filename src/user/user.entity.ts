da3209b6cfea2e8f40dd06ec62323e24d0062438
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-10 00:09:36 +0900

    ADD: (user) email 필드 추가 및 unique 키 설정

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { CommonEntity } from 'src/common/common.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, Unique } from 'typeorm';
 
 @ObjectType({ isAbstract: true })
 @InputType({ isAbstract: true })
@@ -8,10 +8,19 @@ abstract class IUser extends CommonEntity {
   @Field(() => String, { description: '유저의 이름' })
   @Column({ type: 'varchar', length: 256, comment: '유저의 이름' })
   name: string;
+
+  @Field(() => String, { description: '유저의 이메일' })
+  @Column({
+    type: 'varchar',
+    length: 256,
+    comment: '유저의 이메일',
+  })
+  email: string;
 }
 
 @ObjectType()
 @Entity()
+@Unique('unique_email_for_user', ['email']) // email을 unique키로 설정했고 중복인 경우 create에서 에러 메세지를 따로 처리하고 있다
 export class User extends IUser {}
 
 @InputType()
