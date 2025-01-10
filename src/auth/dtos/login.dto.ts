9874ea99245098737c3be8dc374503fd4e93c10c
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-10 23:53:40 +0900

    ADD: (auth - login) dto

---
+++ src/auth/dtos/login.dto.ts
@@ -0,0 +1,5 @@
+import { InputType, PickType } from '@nestjs/graphql';
+import { UserInputType } from 'src/user/user.entity';
+
+@InputType()
+export class LoginInputDto extends PickType(UserInputType, ['email']) {}
