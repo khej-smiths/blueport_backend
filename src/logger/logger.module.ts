64e30599be549197f3a2f136f46be1812f6c4a2c
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-11 00:04:13 +0900

    FIX: (logger) 전역으로 설정, 로거 섯비스 클래스 export로 설정

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { LoggerStorage } from './logger-storage';
 import { LoggerInterceptor } from './logger.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CustomLogger } from './logger';
 
+@Global()
 @Module({
   providers: [
+    CustomLogger,
     LoggerStorage,
     { provide: APP_INTERCEPTOR, useClass: LoggerInterceptor },
   ],
-  exports: [LoggerStorage],
+  exports: [LoggerStorage, CustomLogger],
 })
 export class LoggerModule {}
