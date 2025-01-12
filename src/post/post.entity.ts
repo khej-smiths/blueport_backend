6f6ec413f2dc83b9a268c53c307ea01d4f25a30c
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-13 00:50:11 +0900

    FIX: (게시글) column 추가, 테이블 하드코딩 추가

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,22 +1,24 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import { CommonEntity } from 'src/common/common.entity';
+import { Column, Entity } from 'typeorm';
 
 @ObjectType({ isAbstract: true })
 @InputType({ isAbstract: true })
-abstract class IPost {
-  @Field(() => Int, { description: '게시글의 id' })
-  id: number;
-
+abstract class IPost extends CommonEntity {
   @Field(() => String, { description: '게시글의 제목' })
+  @Column({ type: 'varchar', length: 512, comment: '게시글의 제목' })
   title: string;
 
   @Field(() => String, { description: '게시글 내용' })
+  @Column({ type: 'longtext', comment: '게시글 내용' })
   content: string;
 
-  @Field(() => Int, { description: '게시글 작성자의 id' })
-  writerId: number;
+  // @Field(() => Int, { description: '게시글 작성자의 id' })
+  // writerId: number;
 }
 
 @ObjectType()
+@Entity('post')
 export class Post extends IPost {}
 
 @InputType()
