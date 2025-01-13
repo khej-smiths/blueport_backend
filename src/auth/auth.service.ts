50b050495fa656a87ec29544cbb755071582696e
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-13 14:59:45 +0900

    FIX: (auth) 토큰 발급

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { LoginInputDto } from './dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { CustomGraphQLError } from 'src/common/error';
+import { JWT_PRIVATE_CLAIMS } from './types';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
   async login(input: LoginInputDto): Promise<string> {
-    return 'token';
+    const ERR_DELETED_USER = 'ERR_DELETED_USER';
+
+    try {
+      // 유저 정보 가져오기
+      const user = await this.userService.readUserByOption({
+        email: input.email,
+      });
+
+      // 해당 유저가 로그인할 수 있는지 확인하기 - 탈퇴 유저는 사용 불가
+      if (user.deletedAt) {
+        throw new Error(ERR_DELETED_USER);
+      }
+
+      // 토큰 생성하기 - payload 생성
+      const payload: JWT_PRIVATE_CLAIMS = {
+        uid: user.id,
+        email: user.email,
+      };
+
+      // 토큰 생성하기
+      const accessToken = await this.jwtService.signAsync(payload);
+
+      // return
+      return accessToken;
+    } catch (e) {
+      // 커스텀해서 만든 에러인 경우 그대로 전달
+      if (e instanceof CustomGraphQLError) {
+        e.message = `[login] - ${e.message}`;
+        throw e;
+      }
+
+      if (e.message === ERR_DELETED_USER) {
+        throw new CustomGraphQLError('탈퇴한 계정입니다.', {
+          extensions: { code: ERR_DELETED_USER },
+        });
+      }
+
+      throw new CustomGraphQLError(e, {
+        extensions: { code: 'UNEXPECTED_ERR' },
+      });
+    }
   }
 }
