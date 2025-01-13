f2bd0bd03ce4b212439f7ad1d0fbc324ec1e1419
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-13 14:27:11 +0900

    FIX: (auth) 모듈에서  jwt 모듈 셋팅

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    UserModule,
+    // jwt 설정
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') },
+      }),
+    }),
+  ],
   providers: [AuthResolver, AuthService],
 })
 export class AuthModule {}
