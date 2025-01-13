ea8619d8366fea4b7d3339292b1bae5d2e585d9f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-13 22:04:20 +0900

    FIX: (auth) 로그인 에러 처리 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,45 +15,28 @@ export class AuthService {
   async login(input: LoginInputDto): Promise<string> {
     const ERR_DELETED_USER = 'ERR_DELETED_USER';
 
-    try {
-      // 유저 정보 가져오기
-      // TODO 없는 이메일인 경우에도 데이터를 조회하고 있어서 로직 수정 필요
-      const user = await this.userService.readUserByOption({
-        email: input.email,
-      });
+    // 유저 정보 가져오기
+    const user = await this.userService.readUserByOption({
+      email: input.email,
+    });
 
-      // 해당 유저가 로그인할 수 있는지 확인하기 - 탈퇴 유저는 사용 불가
-      if (user.deletedAt) {
-        throw new Error(ERR_DELETED_USER);
-      }
-
-      // 토큰 생성하기 - payload 생성
-      const payload: JWT_PRIVATE_CLAIMS = {
-        uid: user.id,
-        email: user.email,
-      };
-
-      // 토큰 생성하기
-      const accessToken = await this.jwtService.signAsync(payload);
-
-      // return
-      return accessToken;
-    } catch (e) {
-      // 커스텀해서 만든 에러인 경우 그대로 전달
-      if (e instanceof CustomGraphQLError) {
-        e.message = `[login] - ${e.message}`;
-        throw e;
-      }
-
-      if (e.message === ERR_DELETED_USER) {
-        throw new CustomGraphQLError('탈퇴한 계정입니다.', {
-          extensions: { code: ERR_DELETED_USER },
-        });
-      }
-
-      throw new CustomGraphQLError(e, {
-        extensions: { code: 'UNEXPECTED_ERR' },
+    // 해당 유저가 로그인할 수 있는지 확인하기 - 탈퇴 유저는 사용 불가
+    if (user.deletedAt) {
+      throw new CustomGraphQLError('탈퇴한 계정입니다.', {
+        extensions: { code: ERR_DELETED_USER },
       });
     }
+
+    // 토큰 생성하기 - payload 생성
+    const payload: JWT_PRIVATE_CLAIMS = {
+      uid: user.id,
+      email: user.email,
+    };
+
+    // 토큰 생성하기
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    // return
+    return accessToken;
   }
 }
