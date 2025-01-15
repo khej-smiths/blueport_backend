99ebe93a64e4c7ea20dcace84e26e4c3a89bf85e
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-16 00:31:59 +0900

    FIX: (user) 유저 생성 및 조회 시 에러 처리 수정

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,24 +16,38 @@ export class UserService {
     private readonly logger: CustomLogger,
   ) {}
 
+  /**
+   * @description 유저 생성
+   * @param input name, email
+   * @returns
+   */
   async createUser(input: CreateUserInputDto): Promise<User> {
+    // 에러의 앞에 달 prefix 선언
+    const errPrefix = `${this.constructor.name} - ${this.createUser.name}`;
+
+    // 이 함수에서 발생하는 에러 케이스 정리
+    const ERR_DUPLICATION_EMAIL = 'ERR_DUPLICATION_EMAIL'; // 이메일이 중복된 경우
+
     try {
+      // 유저 생성
       const user = await this.userRepository.createUser(input);
       return user;
-    } catch (e) {
+    } catch (error) {
       // user 테이블에서 email을 고유키로 설정해둠
-      if (e.code === 'ER_DUP_ENTRY') {
-        throw new CustomGraphQLError(
+      if (error.code === 'ER_DUP_ENTRY') {
+        error = new CustomGraphQLError(
           '유저의 이메일이 중복되어 확인이 필요합니다.',
           {
-            extensions: { code: 'ERR_DUPLICATION_EMAIL' },
+            extensions: { code: ERR_DUPLICATION_EMAIL },
           },
         );
       }
-      // 잡지못한 에러는 아래와 같은 형태로 날리기
-      throw new CustomGraphQLError(e, {
-        extensions: { code: 'ERR_UNEXPECTED' },
-      });
+
+      if (error.extensions?.customFlag) {
+        error.addBriefStacktraceToCode(errPrefix);
+      }
+
+      throw error;
     }
   }
 
@@ -46,47 +60,62 @@ export class UserService {
     userId?: number;
     email?: string;
   }): Promise<User> {
-    // option으로 들어온 항목을 카운트
-    const keyListLength = Object.keys(option).length;
+    // 에러의 앞에 달 prefix 선언
+    const errPrefix = `${this.constructor.name} - ${this.readUserByOption.name}`;
 
-    // 옵션이 1개도 선택되지 않은 경우 에러 처리
-    if (!option || keyListLength === 0) {
-      throw new CustomGraphQLError(
-        '유저 조회를 위한 옵션이 설정되지 않았습니다.',
-        {
-          extensions: { code: 'NO_OPTION' },
-        },
-      );
-    }
+    // 이 함수에서 발생하는 에러 케이스 정리
+    const ERR_NO_OPTION = 'ERR_NO_OPTION'; // 유저 조회를 위한 옵션이 알맞게 설정되지 않은 경우
+    const ERR_NO_USER = 'ERR_NO_USER'; // 유저가 조회되지 않은 경우
+    const ERR_MULTIPLE_USER = 'ERR_MULTIPLE_USER'; // 조회된 유저가 여럿인 경우
 
-    // 유저 조회
-    const userList = await this.userRepository.readUserList({
-      where: {
-        ...(option.userId && { id: option.userId }),
-        ...(option.email && { email: option.email }),
-      },
-    });
+    try {
+      // option으로 들어온 항목을 카운트
+      const keyListLength = Object.keys(option).length;
 
-    if (
-      // 유저가 없는 경우 에러 처리
-      !userList ||
-      userList.length === 0
-    ) {
-      throw new CustomGraphQLError('유저가 조회되지 않습니다.', {
-        extensions: {
-          code: `[${this.constructor.name}-${this.readUserByOption.name}] > NO_USER`,
+      // 옵션이 1개도 선택되지 않은 경우 에러 처리
+      if (!option || keyListLength === 0) {
+        throw new CustomGraphQLError(
+          '유저 조회를 위한 옵션이 설정되지 않았습니다.',
+          {
+            extensions: { code: ERR_NO_OPTION },
+          },
+        );
+      }
+
+      // 유저 조회
+      const userList = await this.userRepository.readUserList({
+        where: {
+          ...(option.userId && { id: option.userId }),
+          ...(option.email && { email: option.email }),
         },
       });
-    } else if (
-      // 유저가 여러개인 경우 에러 처리
-      userList.length > 1
-    ) {
-      throw new CustomGraphQLError('조건에 맞는 유저가 여러명입니다.', {
-        extensions: { code: 'MULTIPLE_USER' },
-      });
-    } else {
-      // 유저가 1개만 조회된 경우 값을 리턴
-      return userList[0];
+
+      if (
+        // 유저가 없는 경우 에러 처리
+        !userList ||
+        userList.length === 0
+      ) {
+        throw new CustomGraphQLError('유저가 조회되지 않습니다.', {
+          extensions: {
+            code: ERR_NO_USER,
+          },
+        });
+      } else if (
+        // 유저가 여러개인 경우 에러 처리
+        userList.length > 1
+      ) {
+        throw new CustomGraphQLError('조건에 맞는 유저가 여러명입니다.', {
+          extensions: { code: ERR_MULTIPLE_USER },
+        });
+      } else {
+        // 유저가 1개만 조회된 경우 값을 리턴
+        return userList[0];
+      }
+    } catch (error) {
+      if (error.extensions.customFlag === true) {
+        error.addBriefStacktraceToCode(errPrefix);
+      }
+      throw error;
     }
   }
 }
