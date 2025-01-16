a498490c5fc997d98c3ac970b52d243044590aaf
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-16 15:54:07 +0900

    ADD: (auth) passport + strategy 조합

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class GlobalGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext): Request {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
+  }
+}
