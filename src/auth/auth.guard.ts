f0ddd7a25c55a2483442f9946db1ab22e9d69f0f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-19 22:37:22 +0900

    FIX: (guard) API 에 따른 토큰 및 권한 확인 로직 추가

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,13 +1,64 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Reflector } from '@nestjs/core';
+import { UserService } from 'src/user/user.service';
+import { AccessRoleType } from 'src/auth/auth.decorator';
 
 @Injectable()
-export class GlobalGuard extends AuthGuard('jwt') {
-  getRequest(context: ExecutionContext): Request {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req;
+export class GlobalGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+    private readonly reflector: Reflector,
+  ) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log('GlobalGuard');
+
+    const ERR_NO_TOKEN = 'ERR_NO_TOKEN';
+
+    const gqlContext = GqlExecutionContext.create(context).getContext(); // * 일반 Http Context 를 graphQL Context 로 변경
+
+    const accessToken = gqlContext['access_token'];
+
+    let validationResult!: { email: string; uid: number };
+
+    // 토큰이 있는 경우 토큰을 확인하기
+    if (accessToken) {
+      validationResult = this.authService.validateToken(accessToken);
+
+      // 토큰 검증 후 이메일과 uid가 없는 경우 false 리턴
+      if (!validationResult.email || validationResult.uid) {
+        return false;
+      }
+    }
+
+    // 권한 확인하기
+    let accessRole =
+      this.reflector.get<AccessRoleType>('AccessRole', context.getHandler()) ||
+      'PUBLIC';
+
+    if (accessRole === 'PUBLIC') {
+      // 'PUBLIC' API인 경우 모두가 사용 가능
+      return true;
+    } else {
+      // 'PUBLIC' API가 아닌 경우 유저를 가져와서 확인해야함
+
+      // PUBLIC API가 아닌데 email, uid 가 없는 경우 에러
+      if (!validationResult.email || validationResult.uid) {
+        return false;
+      }
+
+      const user = this.userService.readUserByOption({
+        email: validationResult.email,
+        userId: validationResult.uid,
+      });
+
+      gqlContext['user'] = user;
+      return true;
+    }
   }
 }
