30f920216a89d1685542c62fa84c30e5cfaf5904
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-19 22:03:27 +0900

    ADD: (jwt) payload 타입 추가

--- src/auth/types.ts
+++ src/auth/types.ts
@@ -7,3 +7,11 @@ export type JWT_PRIVATE_CLAIMS = Record<
     'uid', // 유저의 아이디
     number
   >;
+
+// 생성된 토큰의 payload
+export type JWT_TOKEN_PAYLOAD = {
+  exp: number; // 토큰 만료 시간
+  iat: number; // 토큰 생성 시간
+  iss: string; // 토큰의 발급자
+  sub: string; // 토큰의 제목
+} & JWT_PRIVATE_CLAIMS;
