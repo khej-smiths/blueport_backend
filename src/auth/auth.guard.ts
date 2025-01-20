4bd88a3f386281a5e137ec5d20ac36191adff1ed
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-20 22:54:59 +0900

    FIX: (guard) 토큰이 유효성 체크 및 존재 유무 확인 에러처리 추가

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 import { Reflector } from '@nestjs/core';
 import { UserService } from 'src/user/user.service';
 import { AccessRoleType } from 'src/auth/auth.decorator';
+import { CustomGraphQLError } from 'src/common/error';
 
 @Injectable()
 export class GlobalGuard implements CanActivate {
@@ -19,6 +20,7 @@ export class GlobalGuard implements CanActivate {
     console.log('GlobalGuard');
 
     const ERR_NO_TOKEN = 'ERR_NO_TOKEN';
+    const ERR_NOT_VALID_TOKEN = 'ERR_NOT_VALID_TOKEN';
 
     const gqlContext = GqlExecutionContext.create(context).getContext(); // * 일반 Http Context 를 graphQL Context 로 변경
 
@@ -26,12 +28,12 @@ export class GlobalGuard implements CanActivate {
 
     let validationResult!: { email: string; uid: number };
 
-    // 토큰이 있는 경우 토큰을 확인하기
+    // 토큰이 있는 경우 유효한 토큰인지 확인하기
     if (accessToken) {
       validationResult = this.authService.validateToken(accessToken);
 
       // 토큰 검증 후 이메일과 uid가 없는 경우 false 리턴
-      if (!validationResult.email || validationResult.uid) {
+      if (!validationResult.email || !validationResult.uid) {
         return false;
       }
     }
@@ -47,11 +49,24 @@ export class GlobalGuard implements CanActivate {
     } else {
       // 'PUBLIC' API가 아닌 경우 유저를 가져와서 확인해야함
 
-      // PUBLIC API가 아닌데 email, uid 가 없는 경우 에러
-      if (!validationResult.email || validationResult.uid) {
-        return false;
+      if (!accessToken) {
+        throw new CustomGraphQLError('로그인이 필요한 서비스입니다.', {
+          extensions: {
+            code: ERR_NO_TOKEN,
+          },
+        });
       }
 
+      // PUBLIC API가 아닌데 email, uid 가 없는 경우 에러
+      if (!validationResult.email || !validationResult.uid) {
+        throw new CustomGraphQLError('유효하지 않은 로그인입니다.', {
+          extensions: {
+            code: ERR_NOT_VALID_TOKEN,
+          },
+        });
+      }
+
+      // 유저 정보 가져오기
       const user = this.userService.readUserByOption({
         email: validationResult.email,
         userId: validationResult.uid,
