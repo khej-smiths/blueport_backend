8451be8be02daccd51327f4d789685b4b8fae1a7
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-01-21 22:13:03 +0900

    FIX: (main) dto 타입 지정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // transform: 네트워크를 통해 넘어오는 페이로드를 지정한 dto 타입에 맞게 객체를 변환
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(7777);
 }
 bootstrap();
