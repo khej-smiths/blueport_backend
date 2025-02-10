6bde5ae16928d617957f2a4340e076cfa825c316
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-02-10 23:45:10 +0900

    FIX: (user) 공부용 주석 추가

--- src/user/user.data-loader.ts
+++ src/user/user.data-loader.ts
@@ -13,7 +13,15 @@ export class UserDataLoaderService {
    * @param userIdList
    * @returns
    */
-  // TODO dataLoader가 여러 키를 하나로 묶어 주는 것 까진 이해를 했는데, 어떻게 load에 키를 다 쌓고나면.... 그걸 알아서 인식해서 DB 콜로 넘어가는건지 모르겠음
+
+  /**
+   * !STUDY
+   * TODO dataLoader에 load로 키 값을 넣어주는 것으로 어떻게 batch로 데이터를 정리해서 db에 호출할 수 있는지 로직을 확인해야함
+   * TODO 노션 정리 필요
+   * @ref https://medium.com/@bhryan1013/n-1%EC%9D%84-%ED%95%B4%EA%B2%B0%ED%95%98%EB%8A%94-dataloader%EC%9D%98-%EB%A7%88%EB%B2%95%EA%B0%99%EC%9D%80-%EC%9D%B4%EC%95%BC%EA%B8%B0-feat-node-js-%EC%9D%98-event-loop-aa740d6dade9
+   * @ref https://medium.com/zigbang/dataloader%EB%A1%9C-non-graphql%ED%99%98%EA%B2%BD%EC%97%90%EC%84%9C-%ED%99%9C%EC%9A%A9%ED%95%98%EA%B8%B0-e6619010f60b
+   * @ref https://www.korecmblog.com/blog/node-js-event-loop
+   */
   readonly getUsersByIds = new DataLoader<string, User>(
     async (userIdList: Array<string>) => {
       console.log('getUsersByIDs', userIdList);
