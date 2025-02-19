f473453d173a6cc73c7f38e98e22bfa0b0e685ba
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-02-19 16:45:53 +0900

    FIX: (wrapper) 원 함수가 비동기인지 동기인지에 따라 구별해서 함수를 감쌀 수 있도록 분기처리

--- src/logger/log.decorator.ts
+++ src/logger/log.decorator.ts
@@ -25,6 +25,9 @@ export const Wrapper: () => ClassDecorator = () => {
         return;
       }
 
+      // 동기함수인지 비동기 함수인지 확인
+      const isAsync = originalMethod.constructor.name === 'AsyncFunction';
+
       /**
        * [일반함수로 표현한 이유]
        * 화살표 함수의 경우 어떤 객체에서 호출되더라도 함수가 만들어질 때의 this를 사용한다.
@@ -32,49 +35,95 @@ export const Wrapper: () => ClassDecorator = () => {
        * 일반 함수로 선언할 경우 함수가 호출된 위치의 this를 사용하기 때문에 class 의 this와 바인딩되어 class 에 있는 Logger를 사용할 수 있다.
        */
       // 현 함수를 바꿔치기하기(logger감싸지고, 에러 처리가 추가된 형태로)
-      target.prototype[method] = async function (...args: any) {
-        // 에러에서 사용할 수 있도록 prefix 우선 할당
-        const prefix = `${this.constructor.name} - ${method}`;
+      target.prototype[method] = isAsync
+        ? async function (...args: any) {
+            // 에러에서 사용할 수 있도록 prefix 우선 할당
+            const prefix = `${this.constructor.name} - ${method}`;
 
-        // als에서 logger 가져오기
-        const customLogger = this.als.getStore()!.customLogger;
+            // als에서 logger 가져오기
+            const customLogger = this.als.getStore()!.customLogger;
 
-        // 해당 클래스에 logger가 있을 경우, input을 로그로 남긴다.
-        if (customLogger) {
-          customLogger.customLog(
-            { input: args },
-            { className: target.name, methodName: method },
-          );
-        }
+            // 해당 클래스에 logger가 있을 경우, input을 로그로 남긴다.
+            if (customLogger) {
+              customLogger.customLog(
+                { input: args },
+                { className: target.name, methodName: method },
+              );
+            }
 
-        // this 바인딩
-        Object.defineProperty(target.prototype[method], 'name', {
-          value: method,
-        });
+            // this 바인딩
+            Object.defineProperty(target.prototype[method], 'name', {
+              value: method,
+            });
 
-        let result: any;
+            let result: any;
 
-        try {
-          // 기존 메소드를 실행한다.
-          result = await originalMethod.apply(this, args);
-          // 해당 클래스에 logger가 있을 경우, output을 로그로 남긴다.
-          if (customLogger) {
-            customLogger.customLog(
-              { output: result },
-              { className: target.name, methodName: method },
-            );
+            try {
+              // 기존 메소드를 실행한다.
+              result = await originalMethod.apply(this, args);
+
+              // 해당 클래스에 logger가 있을 경우, output을 로그로 남긴다.
+              if (customLogger) {
+                customLogger.customLog(
+                  { output: result },
+                  { className: target.name, methodName: method },
+                );
+              }
+            } catch (error) {
+              // 만약 에러가 발생한 경우, 그 에러가 custom으로 생성된 에러인 경우 prefix 를 추가한다.
+              if (error.extensions?.customFlag) {
+                error.addBriefStacktraceToCode(prefix);
+              }
+              throw error;
+            }
+
+            // 실제 함수의 결과를 리턴
+            return result;
           }
-        } catch (error) {
-          // 만약 에러가 발생한 경우, 그 에러가 custom으로 생성된 에러인 경우 prefix 를 추가한다.
-          if (error.extensions?.customFlag) {
-            error.addBriefStacktraceToCode(prefix);
-          }
-          throw error;
-        }
+        : function (...args: any) {
+            // 에러에서 사용할 수 있도록 prefix 우선 할당
+            const prefix = `${this.constructor.name} - ${method}`;
 
-        // 실제 함수의 결과를 리턴
-        return result;
-      };
+            // als에서 logger 가져오기
+            const customLogger = this.als.getStore()!.customLogger;
+
+            // 해당 클래스에 logger가 있을 경우, input을 로그로 남긴다.
+            if (customLogger) {
+              customLogger.customLog(
+                { input: args },
+                { className: target.name, methodName: method },
+              );
+            }
+
+            // this 바인딩
+            Object.defineProperty(target.prototype[method], 'name', {
+              value: method,
+            });
+
+            let result: any;
+
+            try {
+              // 기존 메소드를 실행한다.
+              result = originalMethod.apply(this, args);
+
+              // 해당 클래스에 logger가 있을 경우, output을 로그로 남긴다.
+              if (customLogger) {
+                customLogger.customLog(
+                  { output: result },
+                  { className: target.name, methodName: method },
+                );
+              }
+            } catch (error) {
+              // 만약 에러가 발생한 경우, 그 에러가 custom으로 생성된 에러인 경우 prefix 를 추가한다.
+              if (error.extensions?.customFlag) {
+                error.addBriefStacktraceToCode(prefix);
+              }
+              throw error;
+            }
+
+            // 실제 함수의 결과를 리턴
+            return result;
+          };
     });
   };
 };
