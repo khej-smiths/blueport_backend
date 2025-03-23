5f66d2991abdd7bb144e0eaa282044ef1e84afb1
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-03-23 12:24:48 +0900

    ADD: (blog) resolver 등록

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogRepository } from './blog.repository';
+import { BlogResolver } from './blog.resolver';
 
 @Module({
-  providers: [BlogService, BlogRepository],
+  providers: [BlogResolver, BlogService, BlogRepository],
 })
 export class BlogModule {}
