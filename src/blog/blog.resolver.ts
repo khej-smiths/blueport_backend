5ccf79a8314d4871ec388798afb63aa1d2070ecc
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-03-23 12:25:48 +0900

    FIX: (blog) 생성 시 접근 권한 확인 및 적용

--- src/blog/blog.resolver.ts
+++ src/blog/blog.resolver.ts
@@ -3,13 +3,19 @@ import { input } from 'src/common/consts';
 import { Blog } from './blog.entity';
 import { BlogService } from './blog.service';
 import { CreateBlogInputDto } from './dtos/create-blog.dto';
+import { AccessRole, AuthUser } from 'src/auth/auth.decorator';
+import { User } from 'src/user/user.entity';
 
 @Resolver(() => Blog)
 export class BlogResolver {
   constructor(private readonly blogService: BlogService) {}
 
+  @AccessRole('USER')
   @Mutation(() => Blog, { description: '블로그 생성' })
-  async createBlog(@Args(input) input: CreateBlogInputDto): Promise<Blog> {
-    return await this.blogService.createBlog(input);
+  async createBlog(
+    @Args(input) input: CreateBlogInputDto,
+    @AuthUser() user: User,
+  ): Promise<Blog> {
+    return await this.blogService.createBlog(input, user);
   }
 }
