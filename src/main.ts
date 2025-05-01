f0c2f99a1f84193272cefa05ed6d8e0e586bef6f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-05-01 22:05:55 +0900

    TODO: lightsail

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   // transform: 네트워크를 통해 넘어오는 페이로드를 지정한 dto 타입에 맞게 객체를 변환
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  // TODO aws lightsail로 웹 서버 및 DB 셋팅
+
   await app.listen(7777);
 }
 bootstrap();
