83fce1e59402b1d1b05f938deb9c48237d48fd30
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-05-07 01:23:46 +0900

    ADD: TODO

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
   // transform: 네트워크를 통해 넘어오는 페이로드를 지정한 dto 타입에 맞게 객체를 변환
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  // TODO aws lightsail로 웹 서버 및 DB 셋팅
+  // TODO https 로 접근되도록 수정하기
+  // TODO orm 바꾸기
+  // TODO 도커, 환경변수 파일들 셋팅 다시하기(gitignore, 파일명 등등)
 
   await app.listen(7777);
 }
