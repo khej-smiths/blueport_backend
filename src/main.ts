f7bd2be812726ab7460805da16af8e47163fb139
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-05-18 20:04:31 +0900

    TODO

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
   // transform: 네트워크를 통해 넘어오는 페이로드를 지정한 dto 타입에 맞게 객체를 변환
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  // TODO ing - 비밀번호 암호화(2025.05.18)
+
   // TODO https 로 접근되도록 수정하기
   // TODO orm 바꾸기
   // TODO 도커, 환경변수 파일들 셋팅 다시하기(gitignore, 파일명 등등)
