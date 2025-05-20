6ef7d851fcfa166eb4766e9a53c13dfb66e96ac0
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-05-21 00:48:38 +0900

    FIX: (auth) 메타데이터 설명 주석으로 추가

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -6,11 +6,16 @@ import {
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/user/user.entity';
 
-export type AccessRoleType = 'PUBLIC' | 'USER' | 'OPTIONAL';
+// API 접근 권한
+export type AccessRoleType =
+  | 'PUBLIC' // 누구나
+  | 'USER' // 회원만
+  | 'OPTIONAL'; // 누구나 가능하지만 회원인 경우 추가 작업 필요
 
 export const AccessRole = (role?: AccessRoleType) =>
   SetMetadata('AccessRole', role || 'USER');
 
+// API별 필요한 정보(유저와 관계가 있는)
 export type RequiredRelationsList = Array<'blog'>;
 
 export const RequiredRelationList = (relationsList?: RequiredRelationsList) =>
