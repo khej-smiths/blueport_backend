01f1006c69ac7e4cc4785141ffed7a14691e9748
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-05-21 00:47:15 +0900

    ADD: (auth) 유저 정보 가져올 때 같이 가져와야할 정보가 있는 경우를 표시하기 위한 메타데이터 추가

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -11,6 +11,11 @@ export type AccessRoleType = 'PUBLIC' | 'USER' | 'OPTIONAL';
 export const AccessRole = (role?: AccessRoleType) =>
   SetMetadata('AccessRole', role || 'USER');
 
+export type RequiredRelationsList = Array<'blog'>;
+
+export const RequiredRelationList = (relationsList?: RequiredRelationsList) =>
+  SetMetadata('RequiredRelationList', relationsList);
+
 export const AuthUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const gqlContext = GqlExecutionContext.create(context).getContext();
