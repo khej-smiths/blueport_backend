b219cc00dea62a8468fd6c9c8a129cb1cfd3cd17
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-05-27 15:45:32 +0900

    FIX: (post) viewCount 추가

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,4 +1,4 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { CommonEntity } from 'src/common/common.entity';
 import { User } from 'src/user/user.entity';
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
@@ -17,6 +17,10 @@ abstract class IPost extends CommonEntity {
   @Field(() => [String], { nullable: true, description: '게시글의 해시태그' })
   @Column({ type: 'json', nullable: true, name: 'hashtag_list' })
   hashtagList?: Array<string>;
+
+  @Field(() => Int, { description: '조회수' })
+  @Column({ type: 'int', unsigned: true, default: 0, comment: '조회수' })
+  viewCount: number;
 
   @Column({ type: 'uuid', name: 'writer_id', comment: '게시글 작성자의 id' })
   writerId: string;
