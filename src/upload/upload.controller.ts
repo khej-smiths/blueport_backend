6b073eef9b8b8852845c82b10e088a622f3479cf
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-05-30 16:05:56 +0900

    FIX: (upload) 라우트 수정, 파일업로드 데코레이터 적용

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UploadService } from './upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
-  @Post('file')
-  async uploadFile(): Promise<string> {
-    return await this.uploadService.uploadFile();
+  @Post('blog-image')
+  @UseInterceptors(
+    // API의 request의 body의 form-data에 선언되어야 할 필드명
+    FileInterceptor('file'),
+  )
+  async uploadFile(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ): Promise<string> {
+    return await this.uploadService.uploadFile(file, 'blog-image');
   }
 }
