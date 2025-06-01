72925ecb08f3bad17e6ab216d3b0b7aa706192fe
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-06-01 15:25:09 +0900

    FIX: (upload) blog-image > post-image

--- src/upload/consts.ts
+++ src/upload/consts.ts
@@ -3,5 +3,5 @@ export const UPLOAD_TYPE_LIST_TOKEN = 'UPLOAD_TYPE_LIST';
 export const UPLOAD_LIMIT_SIZE_OBJ_TOKEN = 'UPLOAD_LIMIT_SIZE_OBJ';
 
 // 업로드하는 경우의 수
-export const UPLOAD_TYPE_LIST = ['blog-image', 'profile-image'] as const;
+export const UPLOAD_TYPE_LIST = ['post-image', 'profile-image'] as const;
 export type UPLOAD_TYPE = (typeof UPLOAD_TYPE_LIST)[number];
