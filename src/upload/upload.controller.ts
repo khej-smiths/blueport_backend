96ff1779397065a6c29e4c9417307b983eee1f4c
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-06-01 15:23:28 +0900

    FIX: (upload) 타입 적용(profile-image, post-image)

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,24 +1,27 @@
 import {
   Controller,
+  Param,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { UPLOAD_TYPE } from './consts';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
-  @Post('blog-image')
+  @Post(':type')
   @UseInterceptors(
     // API의 request의 body의 form-data에 선언되어야 할 필드명
     FileInterceptor('file'),
   )
   async uploadFile(
+    @Param('type') type: UPLOAD_TYPE,
     @UploadedFile()
     file: Express.Multer.File,
   ): Promise<string> {
-    return await this.uploadService.uploadFile(file, 'blog-image');
+    return await this.uploadService.uploadFile(file, type);
   }
 }
--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import {
+  UPLOAD_LIMIT_SIZE_OBJ_TOKEN,
+  UPLOAD_TYPE_LIST,
+  UPLOAD_TYPE_LIST_TOKEN,
+} from './consts';
+
+// 파일을 업로드하는 경우
 
 @Module({
-  providers: [UploadService],
+  providers: [
+    UploadService,
+    {
+      // 파일을 업로드하는 경우
+      provide: UPLOAD_TYPE_LIST_TOKEN,
+      useValue: UPLOAD_TYPE_LIST,
+    },
+    {
+      // 업로드 타입별 용량 제한
+      provide: UPLOAD_LIMIT_SIZE_OBJ_TOKEN,
+      useValue: {
+        'blog-image': 8 * 1024 * 1024, // 8MB
+        'profile-image': 3 * 1024 * 1024, // 3  MB
+      },
+    },
+  ],
   controllers: [UploadController],
 })
 export class UploadModule {}
