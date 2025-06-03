67b94d2939974b1cd0788f0f5ba1f56e4da4a385
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-06-03 18:06:25 +0900

    FIX: (resume) education repository 적용

--- src/resume/resume.module.ts
+++ src/resume/resume.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { ResumeResolver } from './resume.resolver';
 import { ResumeService } from './resume.service';
 import { ResumeRepository } from './resume.repository';
+import { EducationRepository } from './repositories/education.repository';
 
 @Module({
-  providers: [ResumeResolver, ResumeService, ResumeRepository],
+  providers: [
+    ResumeResolver,
+    ResumeService,
+    ResumeRepository,
+    EducationRepository,
+  ],
 })
 export class ResumeModule {}
