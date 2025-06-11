0d66e419997069b8b35a895b5fd1d3fa4d40e6f2
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-06-11 18:05:35 +0900

    FIX: (resume) 이력서 생성 시 로그인 정보 확인

--- src/resume/resume.resolver.ts
+++ src/resume/resume.resolver.ts
@@ -3,16 +3,20 @@ import { ResumeService } from './resume.service';
 import { Resume } from './entities/resume.entity';
 import { input } from 'src/common/consts';
 import { CreateResumeInputDto } from './dtos/create-resume.dto';
+import { AccessRole, AuthUser } from 'src/auth/auth.decorator';
+import { User } from 'src/user/user.entity';
 
 @Resolver()
 export class ResumeResolver {
   constructor(private readonly resumeService: ResumeService) {}
   // TODO 이력서 생성
+  @AccessRole('USER')
   @Mutation(() => Resume, { description: '이력서 생성' })
   async createResume(
     @Args(input) input: CreateResumeInputDto,
+    @AuthUser() user: User,
   ): Promise<Resume> {
-    return await this.resumeService.createResume(input);
+    return await this.resumeService.createResume(user, input);
   }
   // TODO 이력서 수정
 }
