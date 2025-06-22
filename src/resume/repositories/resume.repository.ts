4ed2d935d063552d787f7fb1d25b79194d12452f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-06-22 22:03:21 +0900

    feat: 이력서 조회할 때, 옵션 미입력한 경우 에러 처리
    - Repository 단에서 에러 처리를 위해 @Wrapper 데코레이터를 적용
    - ResumeRepository에서 적용한 @Wrapper 데코레이터에서 에러 처리를 위해 LoggerStorage 주입
    - readResumeList의 input.option에서 입력받은 필드가 없는 경우 에러처리

--- src/resume/repositories/resume.repository.ts
+++ src/resume/repositories/resume.repository.ts
@@ -2,10 +2,17 @@ import { DataSource, EntityManager, Repository } from 'typeorm';
 import { Resume } from '../entities/resume.entity';
 import { Injectable } from '@nestjs/common';
 import { ReadResumeInputDto } from '../dtos/read-resume.dto';
+import { CustomGraphQLError } from 'src/common/error';
+import { Wrapper } from 'src/logger/log.decorator';
+import { LoggerStorage } from 'src/logger/logger-storage';
 
 @Injectable()
+@Wrapper()
 export class ResumeRepository extends Repository<Resume> {
-  constructor(private dataSource: DataSource) {
+  constructor(
+    private dataSource: DataSource,
+    private readonly als: LoggerStorage,
+  ) {
     super(Resume, dataSource.createEntityManager());
   }
 
@@ -37,6 +44,17 @@ export class ResumeRepository extends Repository<Resume> {
     option: ReadResumeInputDto;
     relations?: Array<string>;
   }) {
+    const ERR_AT_LEAST_ONE_FIELD = 'ERR_AT_LEAST_ONE_FIELD'; // 입력한 input 옵션이 없는 경우
+
+    if (Object.keys(input.option).length === 0) {
+      throw new CustomGraphQLError(
+        '이력서 조회를 위한 옵션을 다시 확인해주세요.',
+        {
+          extensions: { code: ERR_AT_LEAST_ONE_FIELD },
+        },
+      );
+    }
+
     return await this.find({
       ...(input.option && {
         where: {
