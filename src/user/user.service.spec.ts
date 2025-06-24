ebe8d22001dee006b50624103dacabac3d7f5dc6
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-06-24 16:51:13 +0900

    test: UserService 테스트 코드 작성 완료
    - readUserByOptions
    - updateUser

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,11 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
-import { DataSource } from 'typeorm';
+import { DataSource, UpdateResult } from 'typeorm';
 import { LoggerStorage } from 'src/logger/logger-storage';
 import { CreateUserInputDto } from './dtos/create-user.dto';
 import { User } from './user.entity';
 import { CustomGraphQLError } from 'src/common/error';
+import { UpdateUserInputDto } from './dtos/update-user.dto';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -101,14 +102,127 @@ describe('UserService', () => {
       }
     });
   });
+
   describe('readUserByOption', () => {
-    it('옵션이 1개도 선택되지 않은 경우 에러 처리', () => {});
-    it('유저가 없는 경우 에러 처리', () => {});
-    it('유저가 여럿 선택된 경우 에러 처리', () => {});
-    it('유저 리턴', () => {});
+    // 생성될 Mock 계정
+    const user: User = {
+      id: 'uuid',
+      hashPassword: jest.fn(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      name: '테스터',
+      email: 'test@test.com',
+      password: 'test_pw_123!',
+    };
+
+    it('옵션이 1개도 선택되지 않은 경우 에러 처리', async () => {
+      // option 객체는 있으나 필드가 없는 경우
+      const option = {};
+
+      try {
+        await userService.readUserByOption(option);
+        fail('옵션이 1개도 선택되지 않은 경우 에러 발생');
+      } catch (error) {
+        // 에러 코드 확인
+        expect(error.extensions.code).toContain('ERR_NO_OPTION');
+        // 에러 인스턴스 확인
+        expect(error).toBeInstanceOf(CustomGraphQLError);
+        // 에러 메세지 확인
+        expect(error.message).toContain(
+          '유저 조회를 위한 옵션이 설정되지 않았습니다.',
+        );
+      }
+    });
+
+    // option이 제대로 들어간 상태에서, 유저가 없는지? 유저가 여럿인지? 유효한지 확인할 때 사용하는 변수
+    const option = {
+      where: { id: 'id' },
+    };
+
+    it('유저가 없는 경우 에러 처리', async () => {
+      // 유저가 하나도 안넘어온 경우
+      jest.spyOn(userRepository, 'readUserList').mockResolvedValue([]);
+
+      try {
+        await userService.readUserByOption(option);
+        fail('유저가 없는 경우 에러 발생');
+      } catch (error) {
+        // 에러 코드 확인
+        expect(error.extensions.code).toContain('ERR_NO_USER');
+        // 에러 인스턴스 확인
+        expect(error).toBeInstanceOf(CustomGraphQLError);
+        // 에러 메세지 확인
+        expect(error.message).toContain('유저가 조회되지 않습니다.');
+      }
+    });
+
+    it('유저가 여럿 선택된 경우 에러 처리', async () => {
+      // 유저가 여러명 넘어온 경우
+      jest
+        .spyOn(userRepository, 'readUserList')
+        .mockResolvedValue([user, user]);
+
+      try {
+        await userService.readUserByOption(option);
+        fail('유저가 여러명 조회된 경우 에러 발생');
+      } catch (error) {
+        // 에러 코드 확인
+        expect(error.extensions.code).toContain('ERR_MULTIPLE_USER');
+        // 에러 인스턴스 확인
+        expect(error).toBeInstanceOf(CustomGraphQLError);
+        // 에러 메세지 확인
+        expect(error.message).toContain('조건에 맞는 유저가 여러명입니다.');
+      }
+    });
+
+    it('유저 리턴', async () => {
+      // 리턴된 유저가 1개인 경우
+      jest.spyOn(userRepository, 'readUserList').mockResolvedValue([user]);
+
+      await expect(userService.readUserByOption(option)).resolves.toEqual(user);
+    });
   });
   describe('updateUser', () => {
-    it('유저가 업데이트 되지 않은 경우 에러 처리', () => {});
-    it('유저 업데이트 후 업데이트된 유저 정보를 리턴', () => {});
+    // 업데이트될 유저의 Mock 계정
+    const user: User = {
+      id: 'uuid',
+      hashPassword: jest.fn(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      name: '테스터',
+      email: 'test@test.com',
+      password: 'test_pw_123!',
+    };
+
+    it('유저가 업데이트 되지 않은 경우 에러 처리', async () => {
+      const updateResult: UpdateResult = { affected: 0 } as any;
+      const input: UpdateUserInputDto = {};
+
+      jest.spyOn(userRepository, 'updateUser').mockResolvedValue(updateResult);
+
+      try {
+        await userService.updateUser(user, input);
+        fail('유저 업데이트 실패');
+      } catch (error) {
+        // 에러 코드 확인
+        expect(error.extensions.code).toContain('ERR_NO_UPDATE');
+        // 에러 인스턴스 확인
+        expect(error).toBeInstanceOf(CustomGraphQLError);
+        // 에러 메세지 확인
+        expect(error.message).toContain('업데이트를 하지 못했습니다.');
+      }
+    });
+    it('유저 업데이트 후 업데이트된 유저 정보를 리턴', async () => {
+      const updateResult: UpdateResult = { affected: 1 } as any;
+      const input: UpdateUserInputDto = { name: '수정된 이름' };
+
+      jest.spyOn(userRepository, 'updateUser').mockResolvedValue(updateResult);
+
+      // 유저 정보 + 업데이트할 정보 같이 리턴
+      await expect(userService.updateUser(user, input)).resolves.toEqual({
+        ...user,
+        ...input,
+      });
+    });
   });
 });
